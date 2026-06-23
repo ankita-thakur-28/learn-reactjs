79508eb1bb0a8bbc08a258bbd2011d57680c5a14
ankita-thakur-28 <ankitathakurmfp@gmail.com>
2026-06-23 18:50:08 +0900

    ep-3-JSX

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },[
-        React.createElement("div",{ id: "child", key: "child1" },[
-        React.createElement("h1", {key: "1"}, "This is Namaste React"),
-        React.createElement("h1", {key: "2"}, "By Ankita"),
-    ]),
-     React.createElement("div",{ id: "child2", key: "child2" },[
-        React.createElement("h1", {key: "1"}, "I'm an h1 tag"),
-        React.createElement("h1", {key: "2"}, "I'm an h2 tag"),
-    ]),
+// JSX - is not HTML in JS.
+// It is HTML/ XML like syntsx.
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+// JSX => Babel transpiles it to React.createElement => Object => HTMLElement(render)
 
-]);
+const jsxHeading = <h1 id="heading">Namste React using JSX</h1>;
 
+// React  Functional Component
+const HeadingComponent = () => {
+    return <h1> Namste React Functional Component</h1>;
+}
 
-
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" },
-    "Hello World from React!"
-);
-
-console.log(heading); //object
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
